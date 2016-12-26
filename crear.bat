8f266eb36b9a709de6396e27ca45fd20c163881f
Luis Esteban Waldman <lwaldman@TRABAJO>
2016-12-26 18:20:45 -0300

    first commit

---
+++ crear.bat
@@ -0,0 +1,6 @@
+echo "# MinisterioTotal" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/LuisWaldman/MinisterioTotal.git
+git push -u origin master
